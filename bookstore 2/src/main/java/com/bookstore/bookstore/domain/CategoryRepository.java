608cd349770050f4e7e823369597326104155c5c
yuliageeva <yuliageeva@gmail.com>
2018-02-20 22:43:30 +0200

    Category entity and Category repository added; add or edit book's category from dropdown menu

---
+++ bookstore 2/src/main/java/com/bookstore/bookstore/domain/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.bookstore.bookstore.domain;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+	List<Category> findByName(String name);
+}
+
